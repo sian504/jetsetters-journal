fc423f6d56cacfbd7e4965295b1290187d9cefc3
Sian Jansen <sianjansen1987@gmail.com>
2024-01-21 18:52:24 +0000

    Added interactive map to Profile page

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,27 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
 });
+
+/*
+    jQuery for Leaflet.JS map initialization
+*/
+
+$(document).ready(function () {
+    // Initialize the map
+    var map = L.map('map').setView([0, 0], 2);
+
+    // Add OpenStreetMap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+     // Add marker for New York City
+    L.marker([40.7128, -74.0060]).addTo(map)
+     .bindPopup('<a href="#"><b>New York City</b></a>');
+
+    // Add marker for Bangkok
+    L.marker([13.7563, 100.5018]).addTo(map)
+     .bindPopup('<a href="#"><b>Bangkok</b></a>');
+
+});
